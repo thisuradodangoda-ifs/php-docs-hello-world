bfcfe0be5d486469c29faf5aee16dde31b1d15b2
Thisura Dodangoda <105212745+thisuradodangoda-ifs@users.noreply.github.com>
2023-08-29 22:07:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
 
-echo "Hello World!";
+$hardcoded_username = "sliit_ead";
+$hardcoded_password = md5("123456");
+
+// Is this a POST request?
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if ($_POST['Username'] == $hardcoded_username && $_POST['Password'] == $hardcoded_password){
+        echo json_encode(array(
+            'success' => true,
+            'message' => 'Login successful!'
+        ));
+        exit();
+    }
+    else{
+        echo json_encode(array(
+            'success' => false,
+            'message' => 'Username or Password is wrong'
+        ));
+        exit();
+    }
+}
